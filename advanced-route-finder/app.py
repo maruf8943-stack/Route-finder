8fa61f24d80782eeb2d7b61ec434c2feb6971ced
Maruf Miah <marufkhan4490@gmail.com>
2026-04-25 00:39:57 +0600

    Update app.py

--- advanced-route-finder/app.py
+++ advanced-route-finder/app.py
@@ -4,7 +4,7 @@ import heapq
 import math
 from collections import deque
 import time
-
+import os
 app = Flask(__name__)
 CORS(app)
 
@@ -398,5 +398,7 @@ def index():
     return render_template("index.html")
 
 
+
+
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
